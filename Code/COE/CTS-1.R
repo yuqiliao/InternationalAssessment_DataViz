28b1b1d07af25fd08b87a37269c23b81f47ea9da
yuqiliao <yuqiliao@hotmail.com>
2019-05-19 09:01:58 -0400

    Update CTS-1 to address internal comments (adding superscript, etc.)

--- Code/COE/CTS-1.R
+++ Code/COE/CTS-1.R
@@ -16,7 +16,7 @@
 # library(cowplot)
 
 # define and load all packages
-reqpkg <- c("ggplot2","scales", "tidyr", "Cairo", "extrafont", "dplyr", "lubridate", "tweenr", "animation", "RColorBrewer", "grid", "gridExtra", "directlabels", "gganimate", "ggrepel", "showtext", "ggplotify", "here", "stringr", "readxl")
+reqpkg <- c("ggplot2","scales", "tidyr", "Cairo", "extrafont", "dplyr", "lubridate", "tweenr", "animation", "RColorBrewer", "grid", "gridExtra", "directlabels", "gganimate", "ggrepel", "showtext", "here", "stringr", "readxl")
 
 sapply(reqpkg, function(pkgi) {
   if (!pkgi %in% installed.packages()) {
@@ -46,7 +46,13 @@ df <- read_excel(path = here("Code", "COE", "Materials", "CTS-1_cleanData.xlsx")
 
 # reshape data
 df <- df %>% 
-  gather(key = Category, value = Value, -Year)
+  gather(key = Category, value = Value, -Year) %>% 
+  # add superscripts for some categories
+  mutate(Category = ifelse(Category %in% "Certificates", "Certificates^1", Category),
+         Category = ifelse(Category %in% "Doctor's", "\"Doctor's\"^2", Category),
+         Category = ifelse(Category %in% "Master's", "\"Master's\"", Category),
+         Category = ifelse(Category %in% "Bachelor's", "\"Bachelor's\"", Category),
+         Category = ifelse(Category %in% "Associate's", "\"Associate's\"", Category)) 
 
 
 ### Plotting ------
@@ -80,7 +86,10 @@ temp$Value <- NULL
 temp <- cbind(temp2, temp)
 tf <- rbind(tf, temp)
 
-
+tf$nudge_y <- ifelse(tf$Category %in% "\"Associate's\"", 15000, 
+                     ifelse(tf$Category %in% c("Certificates^1"),-25000, 
+                            ifelse(tf$Category %in% c("\"Master's\""),-10000, 0)))
+#tf$nudge_x <- ifelse(tf$Category %in% "\"Associate's\"", 5, ifelse(tf$Category %in% c("Certificates^1"),-5, 0))
 
 
 
@@ -97,9 +106,27 @@ cols <- c("#fbab18", "#3EC7F4", "#3FA66C","#242953", "#fb3a18")
 
 
 # define title/caption, etc.
-plotCaption <- expression(atop('SOURCE: U.S. Department of Education, National Center for Education Statistics, Integrated Postsecondary Education Data System (IPEDS),',"Fall 2001 through Fall 2017, Completions component. See,"~italic("Digest of Education Statistics 2018")~', Table 318.40.                                                        '))
+#plotCaption <- expression(atop('SOURCE: U.S. Department of Education, National Center for Education Statistics, Integrated Postsecondary Education Data System (IPEDS),',"Fall 2001 through Fall 2017, Completions component. See,"~italic("Digest of Education Statistics 2018")~', Table 318.40.                                                        '))
+
+#plotCaption <- expression(paste(''^{14}, "C", sep = ""))
+#plotCaption <- expression(textstyle(paste(''^{14}, "C", sep = "")))
 
 
+plotCaption <- expression(
+  #line 1
+  atop(textstyle(paste(''^1,"Data are for certificates below the associate's degree level.                                                                                                                                                                                                      ", sep = "")),
+       #line 2
+       atop(textstyle(paste(''^2,"Includes Ph.D., Ed.D., and comparable degrees at the doctoral level. Includes most degrees formerly classified as first-professional, such as M.D., D.D.S., and law degrees.", sep = "")),
+            #line 3 (use scriptscriptstyle to add space between 2 & 3)
+            atop(scriptscriptstyle(""), atop(textstyle("SOURCE: U.S. Department of Education, National Center for Education Statistics, Integrated Postsecondary Education Data System (IPEDS), Fall 2001 through Fall 2017,     "),
+                #line 4 (use scriptscriptstyle to add space between 3 & 4)
+                atop(scriptscriptstyle(""), textstyle("Completions component. See"~italic("Digest of Education Statistics 2018,")~"Table 318.40.                                                                                                                                                                  "))
+                   )
+                )
+            )
+  )
+)
+            
 
 # plotCaption <- list()
 # plotNote <- expression('NOTE: Education systems are ordered by the percentage of students reaching the '~italic('Advanced')~' international benchmark.')
@@ -128,7 +155,7 @@ theme_white <- theme(text = element_text(family="Calibri", color = "black"),
                      axis.ticks.y = element_blank(),
                      plot.title=element_text(size=31,family = "Calibri", face = "bold" , hjust= 0,lineheight=1, margin = margin(t = 15)),
                      plot.subtitle=element_text(size=26, margin = margin(t=15, b = 5),family = "Calibri", face = "plain"),
-                     plot.caption=element_text(size=18, hjust = 0,margin=margin(t=15, b = 15),lineheight=1.15, family = "Calibri"),
+                     plot.caption=element_text(size=17, hjust = 0,margin=margin(t=15, b = 15),lineheight=1.15, family = "Calibri"),
                      #strip.text.x = element_text(size=18, angle = 0, hjust = .5, family = "Calibri Light"),
                      #strip.background = element_rect(fill = "#f1f1f1", colour = NA),
                      legend.position="none"
@@ -161,41 +188,41 @@ yAxisLimits <- c(0,max(yAxisBreaks)* 1.03)
 
 
 
-# plotting (to see how it looks before animation)
-wholeFig <- ggplot(tf, aes(x = Year, y = Value)) +
-  geom_point(data = subset(tf, .frame == min(.frame)),aes(group=Category, color=Category), size=10) + 
-  geom_point(data = subset(tf, .frame == max(.frame)),aes(group=Category, color=Category), size=10) +
-  geom_line(aes(group=Category, color=Category), size=2.5) +
-  scale_x_date(labels=xAxisLabels, expand = c(0.01, 0), breaks=xAxisBreaks,limits =as.Date(c("2000-02-25", "2016-07-21"))) +                           
-  scale_y_continuous(labels=yAxisLabels, expand = c(0, 0), breaks=yAxisBreaks,limits = yAxisLimits) +
-  # geom_text(data = subset(tf, .frame == min(.frame)), size = 9, 
-  #           aes(label = round(Value, 0)),
-  #           nudge_y = subset(tf, .frame == min(.frame))$nudge_y, 
-  #           nudge_x= subset(tf, .frame == min(.frame))$nudge_x, 
-  #           family="Calibri") + 
-  # geom_text(data = subset(tf, .frame == max(.frame)), size = 9, 
-  #           aes(label = round(Value, 0)),
-  #           nudge_y = subset(tf, .frame == max(.frame))$nudge_y, 
-  #           nudge_x= subset(tf, .frame == max(.frame))$nudge_x, 
-  #           family="Calibri") +
-  theme_minimal() + theme_white + 
-  scale_color_manual(values=cols) + scale_fill_manual(values=cols) +
-  labs(x="Year", y="", title = plotTitle, subtitle = plotSubtitle, caption = plotCaption) +
-  geom_dl(data = tf,
-          aes(label = Category), method = list("last.points", cex = 2, hjust = -0.5))
-
-wholeFig
-
-
-# the below code to change plot layout (for better alignment) won't work with gganimate, so give up the gganimate for now
-grid.newpage()
-# ggplotGrob is used to capture the figure in the graphics device, then shift the plot labels to the left most part of the plot
-g <- ggplotGrob(wholeFig)
-g$layout$l[g$layout$name == "title"] <- 4
-g$layout$l[g$layout$name == "caption"] <- 4
-g$layout$l[g$layout$name == "subtitle"] <- 4
-#g$layout$l[g$layout$name == "guide-box"] <- 4
-grid::grid.draw(g);
+# # plotting (to see how it looks before animation)
+# wholeFig <- ggplot(tf, aes(x = Year, y = Value)) +
+#   geom_point(data = subset(tf, .frame == min(.frame)),aes(group=Category, color=Category), size=10) + 
+#   geom_point(data = subset(tf, .frame == max(.frame)),aes(group=Category, color=Category), size=10) +
+#   geom_line(aes(group=Category, color=Category), size=2.5) +
+#   scale_x_date(labels=xAxisLabels, expand = c(0.01, 0), breaks=xAxisBreaks,limits =as.Date(c("2000-02-25", "2016-07-21"))) +                           
+#   scale_y_continuous(labels=yAxisLabels, expand = c(0, 0), breaks=yAxisBreaks,limits = yAxisLimits) +
+#   # geom_text(data = subset(tf, .frame == min(.frame)), size = 9, 
+#   #           aes(label = round(Value, 0)),
+#   #           nudge_y = subset(tf, .frame == min(.frame))$nudge_y, 
+#   #           nudge_x= subset(tf, .frame == min(.frame))$nudge_x, 
+#   #           family="Calibri") + 
+#   # geom_text(data = subset(tf, .frame == max(.frame)), size = 9, 
+#   #           aes(label = round(Value, 0)),
+#   #           nudge_y = subset(tf, .frame == max(.frame))$nudge_y, 
+#   #           nudge_x= subset(tf, .frame == max(.frame))$nudge_x, 
+#   #           family="Calibri") +
+#   theme_minimal() + theme_white + 
+#   scale_color_manual(values=cols) + scale_fill_manual(values=cols) +
+#   labs(x="Year", y="", title = plotTitle, subtitle = plotSubtitle, caption = plotCaption) +
+#   geom_dl(data = tf,
+#           aes(label = Category), method = list("last.points", cex = 2, hjust = -0.5))
+# 
+# wholeFig
+# 
+# 
+# # the below code to change plot layout (for better alignment) won't work with gganimate, so give up the gganimate for now
+# grid.newpage()
+# # ggplotGrob is used to capture the figure in the graphics device, then shift the plot labels to the left most part of the plot
+# g <- ggplotGrob(wholeFig)
+# g$layout$l[g$layout$name == "title"] <- 4
+# g$layout$l[g$layout$name == "caption"] <- 4
+# g$layout$l[g$layout$name == "subtitle"] <- 4
+# #g$layout$l[g$layout$name == "guide-box"] <- 4
+# grid::grid.draw(g);
 
 
 
@@ -223,75 +250,6 @@ gifReplicate <- function(x) {
   grid.draw(x)
 }
 
-# version 1 - pause in each year (and draw dots) - not used =====
-saveGIF({
-  print(Sys.time())
-  for (i in 1:max(tf$.frame)) {
-    print(paste0("working on the ", i, "th frame"))
-    g <- ggplot(data = subset(tf, .frame <= i), aes(x = Year, y = Value, .frame = i)) +
-      geom_line(aes(group=Category, color=Category), size=2.5) +
-      scale_x_date(labels=xAxisLabels, expand = c(0.01, 0), breaks=xAxisBreaks,limits =as.Date(c("2000-02-25", "2018-10-21"))) +                           
-      scale_y_continuous(labels=yAxisLabels, expand = c(0, 0), breaks=yAxisBreaks,limits = yAxisLimits) +
-      theme_minimal() + theme_white + 
-      scale_color_manual(values=cols) + scale_fill_manual(values=cols) +
-      labs(x="Year", y="", title = plotTitle, subtitle = plotSubtitle, 
-           caption = plotCaption) 
-    
-    # when i == 1, geom_dl wont' work
-    if(i!=1){
-      g <- g +
-        geom_dl(data = subset(tf, .frame <= i),
-                aes(label = Category), method = list("last.qp", cex = 2, hjust = -0.2))
-    }
-    
-    
-    # add geom_point to g based on i
-    X_axis_years <- unique(df$Year)
-    Current_years <- unique(subset(tf, .frame <= i)$Year)
-    a <- X_axis_years %in% Current_years
-    
-    g <- g + geom_point(data = subset(tf, Year %in% X_axis_years[a]),aes(group=Category, color=Category), size=6)
-      # # add geom_text to g based on i
-      # geom_text(data = subset(tf, Year %in% X_axis_years[a]), size = 9, 
-      #           aes(label = round(Value, 0)),
-      #           nudge_y = subset(tf, .frame == min(.frame))$nudge_y, 
-      #           nudge_x= subset(tf, .frame == min(.frame))$nudge_x, 
-      #           family="Calibri")
-    
-    #grob
-    plot <- ggplotGrob(g)
-    
-    # define object that store the name of frame with which we want the plot to "pause"
-    pause_frames <- unique(tf[tf$Year %in% X_axis_years , ]$.frame)
-    
-    
-    
-    
-    # draw the plot
-    if (i %in% pause_frames){
-      # replicate (and draw) the plot many times
-      grid::grid.draw(plot);
-      # let the last plot pause a bit more
-      if (i == pause_frames[length(pause_frames)]){
-        replicate(200,gifReplicate(plot))
-      } else {
-        replicate(10,gifReplicate(plot))
-      }
-      
-    } else {
-      # just draw the plot one time
-      grid::grid.draw(plot);
-      grid.newpage()
-    }
-    
-    
-  }  
-  print(Sys.time())
-},
-# specify the pathway and name of the gif output, as well as the interval, width, and height
-movie.name=here("Code", "COE", "Results", "CTS-1_v1.gif"),interval = .02, ani.width = 1200, ani.height = 1000)
-
-
 # version 2 - pause only at begining and end =====
 saveGIF({
   print(Sys.time())
@@ -309,8 +267,7 @@ saveGIF({
     # when i == 1, geom_dl wont' work
     if(i!=1){
       g <- g +
-        geom_dl(data = subset(tf, .frame <= i),
-                aes(label = Category), method = list("last.qp", cex = 2, hjust = -0.2))
+        geom_text(data = subset(tf, .frame == i), aes(label = Category), hjust=-0.2, vjust=0, show.legend = FALSE, parse=TRUE, size = 7, nudge_y = subset(tf, .frame == i)$nudge_y)
     }
     
     
@@ -369,13 +326,82 @@ saveGIF({
     }
     
     
+  }
+  print(Sys.time())
+},
+# specify the pathway and name of the gif output, as well as the interval, width, and height
+movie.name=here("Code", "COE", "Results", "CTS-1_v3.gif"),interval = .02, ani.width = 1200, ani.height = 800)
+
+
+
+
+ # version 1 - pause in each year (and draw dots) - not used =====
+saveGIF({
+  print(Sys.time())
+  for (i in 1:max(tf$.frame)) {
+    print(paste0("working on the ", i, "th frame"))
+    g <- ggplot(data = subset(tf, .frame <= i), aes(x = Year, y = Value, .frame = i)) +
+      geom_line(aes(group=Category, color=Category), size=2.5) +
+      scale_x_date(labels=xAxisLabels, expand = c(0.01, 0), breaks=xAxisBreaks,limits =as.Date(c("2000-02-25", "2018-10-21"))) +                           
+      scale_y_continuous(labels=yAxisLabels, expand = c(0, 0), breaks=yAxisBreaks,limits = yAxisLimits) +
+      theme_minimal() + theme_white + 
+      scale_color_manual(values=cols) + scale_fill_manual(values=cols) +
+      labs(x="Year", y="", title = plotTitle, subtitle = plotSubtitle, 
+           caption = plotCaption) 
+    
+    # when i == 1, geom_dl wont' work
+    if(i!=1){
+      g <- g +
+        geom_dl(data = subset(tf, .frame <= i),
+                aes(label = Category), method = list("last.qp", cex = 2, hjust = -0.2))
+    }
+    
+    
+    # add geom_point to g based on i
+    X_axis_years <- unique(df$Year)
+    Current_years <- unique(subset(tf, .frame <= i)$Year)
+    a <- X_axis_years %in% Current_years
+    
+    g <- g + geom_point(data = subset(tf, Year %in% X_axis_years[a]),aes(group=Category, color=Category), size=6)
+    # # add geom_text to g based on i
+    # geom_text(data = subset(tf, Year %in% X_axis_years[a]), size = 9, 
+    #           aes(label = round(Value, 0)),
+    #           nudge_y = subset(tf, .frame == min(.frame))$nudge_y, 
+    #           nudge_x= subset(tf, .frame == min(.frame))$nudge_x, 
+    #           family="Calibri")
+    
+    #grob
+    plot <- ggplotGrob(g)
+    
+    # define object that store the name of frame with which we want the plot to "pause"
+    pause_frames <- unique(tf[tf$Year %in% X_axis_years , ]$.frame)
+    
+    
+    
+    
+    # draw the plot
+    if (i %in% pause_frames){
+      # replicate (and draw) the plot many times
+      grid::grid.draw(plot);
+      # let the last plot pause a bit more
+      if (i == pause_frames[length(pause_frames)]){
+        replicate(200,gifReplicate(plot))
+      } else {
+        replicate(10,gifReplicate(plot))
+      }
+      
+    } else {
+      # just draw the plot one time
+      grid::grid.draw(plot);
+      grid.newpage()
+    }
+    
+    
   }  
   print(Sys.time())
 },
 # specify the pathway and name of the gif output, as well as the interval, width, and height
-movie.name=here("Code", "COE", "Results", "CTS-1_v2_2.gif"),interval = .02, ani.width = 1200, ani.height = 800)
-
-
+movie.name=here("Code", "COE", "Results", "CTS-1_v1.gif"),interval = .02, ani.width = 1200, ani.height = 1000)
 
 
 
